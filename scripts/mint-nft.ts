7b5816c2176ae39e02f37c0060764fd4009776e9
Javier Donoso <javier.donoso@nuilea.com>
2023-07-11 17:36:03 +0200

    readme

--- scripts/mint-nft.ts
+++ scripts/mint-nft.ts
@@ -1,17 +1,18 @@
 import { deployments, ethers } from "hardhat";
 import { GaslessNFT } from "../typechain";
 import { NATIVE_TOKEN } from "../src/constants";
-import { CallWithERC2771Request, CallWithSyncFeeRequest, GelatoRelay } from "@gelatonetwork/relay-sdk";
-
-
+import {
+  CallWithERC2771Request,
+  CallWithSyncFeeRequest,
+  GelatoRelay,
+} from "@gelatonetwork/relay-sdk";
 
 const main = async () => {
   const PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY || "";
-
-if (!PRIVATE_KEY)
-  throw "⛔️ Private key not detected! Add it to the .env file!";
-
-
+  const sponsorKey = process.env.SPONSOR_KEY || "";
+  
+  if (!PRIVATE_KEY)
+    throw "⛔️ Private key not detected! Add it to the .env file!";
 
   // Initialize the wallet.
   const wallet = new ethers.Wallet(PRIVATE_KEY);
@@ -24,16 +25,19 @@ if (!PRIVATE_KEY)
   const { data } = await nft.populateTransaction.mint();
   if (!data) throw new Error("Invalid transaction");
 
-  const request:  CallWithERC2771Request= {
+  const request: CallWithERC2771Request = {
     target: nft.address,
     data: data,
     chainId: chainId,
-    user:deployer.address
+    user: deployer.address,
   };
 
-
   const relay = new GelatoRelay();
-  const { taskId } = await relay.sponsoredCallERC2771(request,wallet,"i_7WlEZIpawFdyuavUqESyoNhjd8pO02xU7VcRifqK4_");
+  const { taskId } = await relay.sponsoredCallERC2771(
+    request,
+    wallet,
+    sponsorKey
+  );
 
   console.log("https://api.gelato.digital/tasks/status/" + taskId);
 };
